af43f02c2c2a32ce7fad8c2ae150f9967f91c47a
Eyal <eyal.sverdlov@gmail.com>
2021-08-24 19:31:58 +0300

    working with files

--- app/src/main/java/com/example/qrft/MainActivity.kt
+++ app/src/main/java/com/example/qrft/MainActivity.kt
@@ -2,8 +2,13 @@ package com.example.qrft
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.ListView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
@@ -12,6 +17,11 @@ import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.qrft.databinding.ActivityMainBinding
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.EncodeHintType
+import com.google.zxing.qrcode.QRCodeWriter
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel
+import java.io.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -19,16 +29,21 @@ class MainActivity : AppCompatActivity() {
     companion object {
         private const val REQUEST_CODE_PERMISSIONS = 10
         private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val QRCODE_SIZE = 1000
     }
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var cameraExecutor: ExecutorService
-    private lateinit var imageAnalysis: ImageAnalysis
+    private lateinit var titleText: EditText
+    private lateinit var editText: EditText
+    private lateinit var filesList : ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        editText = findViewById(R.id.editText)
+        titleText = findViewById(R.id.titleText)
 
         cameraExecutor = Executors.newSingleThreadExecutor()
         imageAnalysis = ImageAnalysis.Builder().build()
@@ -41,6 +56,8 @@ class MainActivity : AppCompatActivity() {
         } else {
             ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
         }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
     override fun onDestroy() {
@@ -71,6 +88,11 @@ class MainActivity : AppCompatActivity() {
             // Used to bind the lifecycle of cameras to the lifecycle owner
             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
 
+            val imageAnalyzer = ImageAnalysis.Builder().build()
+            imageAnalyzer.setAnalyzer(cameraExecutor, QRCodeAnalyzer {
+                Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+            })
+
             try {
                 // Unbind use cases before rebinding
                 cameraProvider.unbindAll()
@@ -81,5 +103,57 @@ class MainActivity : AppCompatActivity() {
                 Log.e(this.toString(), "Use case binding failed", ex)
             }
         }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun generateQRCode(contents: String) {
+        val hints = mapOf(
+            EncodeHintType.ERROR_CORRECTION to ErrorCorrectionLevel.H
+        )
+        val bitMatrix = QRCodeWriter().encode(contents, BarcodeFormat.QR_CODE, QRCODE_SIZE, QRCODE_SIZE, hints)
+        val bitmap = Bitmap.createBitmap(QRCODE_SIZE, QRCODE_SIZE, Bitmap.Config.RGB_565)
+        for (y in 0 until QRCODE_SIZE) {
+            for (x in 0 until QRCODE_SIZE) {
+                bitmap.setPixel(x, y, if (bitMatrix[x, y]) Color.BLACK else Color.WHITE)
+            }
+        }
+        binding.qrcode.setImageBitmap(bitmap)
+    }
+
+    fun saveTextFile(view: View) {
+        try {
+            val filename = titleText.text.toString() + ".txt"
+            val fileOutputStream: FileOutputStream =
+                openFileOutput(filename, Context.MODE_PRIVATE)
+            val outputWriter = OutputStreamWriter(fileOutputStream)
+            outputWriter.write(editText.text.toString())
+            outputWriter.close()
+            Toast.makeText(baseContext, "File saved successfully!", Toast.LENGTH_SHORT).show()
+        }
+        catch (e: Exception) {
+            Toast.makeText(baseContext, "Error, could not save file!", Toast.LENGTH_SHORT).show()
+            e.printStackTrace()
+        }
+    }
+
+    fun readTextFile(view: View) {
+        try {
+            val fileInputStream: FileInputStream =
+                openFileInput(titleText.text.toString() + ".txt")
+            val inputStreamReader: InputStreamReader = InputStreamReader(fileInputStream)
+            val bufferedReader: BufferedReader = BufferedReader(inputStreamReader)
+            val stringBuilder: StringBuilder = StringBuilder();
+            var text: String? = null;
+            while(run {
+                    text = bufferedReader.readLine()
+                    text
+                } != null) {
+                stringBuilder.append(text)
+            }
+            editText.setText(stringBuilder.toString()).toString()
+        }
+        catch (e: Exception) {
+            Toast.makeText(baseContext, "Error, could not read file!", Toast.LENGTH_SHORT).show()
+            e.printStackTrace()
+        }
     }
 }
